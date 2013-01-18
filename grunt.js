69cea5d49c47dae9766f16a365ab0ab35a5745c0
John White <jonttyy@gmail.com>
2013-01-18 14:20:07 +0000

    Update the grunt file to work with grunt v0.3.9

--- grunt.js
+++ grunt.js
@@ -1,35 +1,37 @@
-config.init({
-  meta : {
-    banner : '/* Wavestreaming.com - https://github.com/Wavestreaming/jquery-shoutcast - MIT licensed */'
-  },
-  lint: {
-    files: ['jquery.shoutcast.js','easySetup.js']
-  },
-  min: {
-    'jquery.shoutcast.min.js': ['<banner>','jquery.shoutcast.js'],
-    'jquery.shoutcast.easy.min.js' : ['<banner>','jquery.shoutcast.js','easySetup.js']
-  },
-  watch: {
-    files: '<config:lint.files>',
-    tasks: 'lint min'
-  },
-  jshint: {
-    options: {
-      expr: true,
-      browser: true,
-      devel: true,
-      strict: true,
-      undef: true,
-      curly: true,
-      eqeqeq: true,
-      forin: true,
-      immed: true,
-      latedef: true,
-      noarg: true,
-      noempty: true,
-      regexp: false,
-      jquery: true,
+module.exports = function(grunt){
+  grunt.initConfig({
+    meta : {
+      banner : '/* Wavestreaming.com - https://github.com/Wavestreaming/jquery-shoutcast - MIT licensed */'
+    },
+    lint: {
+      files: ['jquery.shoutcast.js','easySetup.js']
+    },
+    min: {
+      'jquery.shoutcast.min.js': ['<banner>','jquery.shoutcast.js'],
+      'jquery.shoutcast.easy.min.js' : ['<banner>','jquery.shoutcast.js','easySetup.js']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint min'
+    },
+    jshint: {
+      options: {
+        expr: true,
+        browser: true,
+        devel: true,
+        strict: true,
+        undef: true,
+        curly: true,
+        eqeqeq: true,
+        forin: true,
+        immed: true,
+        latedef: true,
+        noarg: true,
+        noempty: true,
+        regexp: false,
+        jquery: true,
+      }
     }
-  }
-});
-task.registerTask('default', 'lint min');
+  });
+  grunt.registerTask('default', 'lint min');
+}
